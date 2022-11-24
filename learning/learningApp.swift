8ddaa0b8a64a8a08ed4613e393b661e9fc284118
Dizzy Farbanish <dfarbanish@thumbtack.com>
2022-11-24 13:04:25 -0800

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by dfarbanish on 11/24/22.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
